650978caebfebdc035c25a028078ec9221f6f6c0
William Fagan <willf@commtech-fastcom.com>
2012-03-22 13:00:30 -0500

    Change licenses to say fc335-linux instead of fscc-linux.

--- examples/c/write.c
+++ examples/c/write.c
@@ -1,20 +1,20 @@
 /*
 	Copyright (C) 2011 Commtech, Inc.
 
-	This file is part of fscc-linux.
+	This file is part of fc335-linux.
 
-	fscc-linux is free software: you can redistribute it and/or modify
+	fc335-linux is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	fscc-linux is distributed in the hope that it will be useful,
+	fc335-linux is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with fscc-linux.	If not, see <http://www.gnu.org/licenses/>.
+	along with fc335-linux.	If not, see <http://www.gnu.org/licenses/>.
 
 */
 
