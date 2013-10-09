9c2a7cefa61a03d3383f6cdf2929c3c3097cdf73
William Fagan <willf@commtech-fastcom.com>
2013-10-09 14:09:03 -0500

    Update rs485 example to manually manipulate Auto 485 since the serial driver will automatically reset it otherwise.

--- examples/rs485.c
+++ examples/rs485.c
@@ -2,19 +2,41 @@
 #include <unistd.h> /* close */
 #include <serialfc.h> /* IOCTL_FASTCOM_* */
 
+/* #define ASYNC_PCIE */
+
 int main(void)
 {
-    int fd = 0;
+    int ttys_fd = 0;
+    int serialfc_fd = 0;
     unsigned status = 0;
+#ifdef ASYNC_PCIE
+    int mode = 0;
+#endif
 
-    fd = open("/dev/serialfc0", O_RDWR);
+    ttys_fd = open("/dev/ttyS4", O_RDWR);
+    serialfc_fd = open("/dev/serialfc0", O_RDWR);
 
-    ioctl(fd, IOCTL_FASTCOM_GET_RS485, &status);
+    ioctl(serialfc_fd, IOCTL_FASTCOM_GET_RS485, &status);
 
-    ioctl(fd, IOCTL_FASTCOM_DISABLE_RS485);
-    ioctl(fd, IOCTL_FASTCOM_ENABLE_RS485);
+    /* Enable RS485 */
+#ifdef ASYNC_PCIE
+    ioctl(ttys_fd, TIOCMGET, &mode);
+    mode |= 0x2000;
+    ioctl(ttys_fd, TIOCMSET, &mode);
+#endif
+    ioctl(serialfc_fd, IOCTL_FASTCOM_ENABLE_RS485);
 
-    close(fd);
+
+    /* Disable RS485 */
+#ifdef ASYNC_PCIE
+    ioctl(ttys_fd, TIOCMGET, &mode);
+    mode &= ~0x2000;
+    ioctl(ttys_fd, TIOCMSET, &mode);
+#endif
+    ioctl(serialfc_fd, IOCTL_FASTCOM_DISABLE_RS485);
+
+    close(serialfc_fd);
+    close(ttys_fd);
 
     return 0;
 }
