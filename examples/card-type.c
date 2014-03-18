8e45c1fcab4417b3c5b834e65ab251a2c3d8cd74
William Fagan <willf@commtech-fastcom.com>
2014-03-18 16:23:01 -0500

    Fix example file naming and add in card-type example.

---
+++ examples/card-type.c
@@ -0,0 +1,17 @@
+#include <fcntl.h> /* open, O_RDWR */
+#include <unistd.h> /* close */
+#include <serialfc.h> /* IOCTL_FASTCOM_* */
+
+int main(void)
+{
+    int fd = 0;
+    unsigned type;
+
+    fd = open("/dev/serialfc0", O_RDWR);
+
+    ioctl(fd, IOCTL_FASTCOM_GET_SAMPLE_RATE, &type);
+
+    close(fd);
+
+    return 0;
+}
