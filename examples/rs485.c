d7b3f446afeea9fa2658df44954bef2be976ae05
William Fagan <willf@commtech-fastcom.com>
2014-03-28 13:55:16 -0500

    Use card type to automatically handle PCIe code in rs485.

--- examples/rs485.c
+++ examples/rs485.c
@@ -2,37 +2,38 @@
 #include <unistd.h> /* close */
 #include <serialfc.h> /* IOCTL_FASTCOM_* */
 
-/* #define ASYNC_PCIE */
-
 int main(void)
 {
     int ttys_fd = 0;
     int serialfc_fd = 0;
     unsigned status = 0;
-#ifdef ASYNC_PCIE
-    int mode = 0;
-#endif
+    unsigned type;
 
     ttys_fd = open("/dev/ttyS4", O_RDWR | O_NDELAY);
     serialfc_fd = open("/dev/serialfc0", O_RDWR);
 
+    ioctl(serialfc_fd, IOCTL_FASTCOM_GET_CARD_TYPE, &type);
     ioctl(serialfc_fd, IOCTL_FASTCOM_GET_RS485, &status);
 
     /* Enable RS485 */
-#ifdef ASYNC_PCIE
-    ioctl(ttys_fd, TIOCMGET, &mode);
-    mode |= 0x2000;
-    ioctl(ttys_fd, TIOCMSET, &mode);
-#endif
+    if (type == SERIALFC_CARD_TYPE_PCIE) {
+        int mode = 0;
+        ioctl(ttys_fd, TIOCMGET, &mode);
+        mode |= 0x2000;
+        ioctl(ttys_fd, TIOCMSET, &mode);
+    }
+
     ioctl(serialfc_fd, IOCTL_FASTCOM_ENABLE_RS485);
 
 
     /* Disable RS485 */
-#ifdef ASYNC_PCIE
-    ioctl(ttys_fd, TIOCMGET, &mode);
-    mode &= ~0x2000;
-    ioctl(ttys_fd, TIOCMSET, &mode);
-#endif
+    if (type == SERIALFC_CARD_TYPE_PCIE) {
+        int mode = 0;
+        ioctl(ttys_fd, TIOCMGET, &mode);
+        mode &= ~0x2000;
+        ioctl(ttys_fd, TIOCMSET, &mode);
+    }
+
     ioctl(serialfc_fd, IOCTL_FASTCOM_DISABLE_RS485);
 
     close(serialfc_fd);
